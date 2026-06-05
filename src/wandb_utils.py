8426041c7c809756a56bbc5aace125eada543bac
anuragvishwakarma02 <61903364+anuragvishwakarma02@users.noreply.github.com>
2026-06-05 19:29:11 +0530

    Update wandb_utils.py

--- src/wandb_utils.py
+++ src/wandb_utils.py
@@ -1,1 +1,29 @@
+"""Shared Weights & Biases initialization helpers."""
 
+import os
+
+import wandb
+
+
+def init_wandb_run(project, name, config=None, resume=None):
+    """Initialize a W&B run using WANDB_API_KEY from environment."""
+    api_key = os.getenv('WANDB_API_KEY')
+
+    init_kwargs = {
+        'project': project,
+        'name': name,
+    }
+    if config is not None:
+        init_kwargs['config'] = config
+    if resume is not None:
+        init_kwargs['resume'] = resume
+
+    if api_key:
+        wandb.login(key=api_key)
+        enabled = True
+    else:
+        init_kwargs['mode'] = 'disabled'
+        enabled = False
+
+    wandb.init(**init_kwargs)
+    return enabled
